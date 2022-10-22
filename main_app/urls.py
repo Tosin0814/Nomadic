80dc2b7584fc3c97554c73bde704e13f0f2e31db
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-10-22 13:36:47 -0400

    Authenticate and create users

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', views.choose_signup, name='choose_signup'),
-    path('accounts/signup_host/', views.signup_host, name='signup_host'),
-    path('accounts/signup_renter/', views.signup_renter, name='signup_renter'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('profile/create/', views.create_profile, name='create_profile')
     # path('accounts/signup/', views.create_profile, name='create_profile'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+
 from django.views.generic import ListView, DeleteView, DetailView, UpdateView, CreateView
 
 from .models import Profile, Property, PropertyFeature, Photo, Availability, Like, Review
@@ -15,14 +15,13 @@ def choose_signup(request):
   return render(request, 'registration/choose_signup.html')
 
 
-def signup_host(request):
+def signup(request):
   error_message = ''
   if request.method == 'POST':
     # Create a 'user' form object that includes the data from the browser
-    form = SignUpForm(request.POST)
+    form = UserCreationForm(request.POST)
     if form.is_valid():
       # Add the user to the database
-      form.type = 'host'
       user = form.save()
       # Login after signing up
       login(request, user)
@@ -30,34 +29,11 @@ def signup_host(request):
     else:
       error_message = 'Invalid sign up - try again'
   # A bad POST or a GET request, so render signup.html with an empty form
-  form = SignUpForm()
+  form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
 
-def signup_renter(request):
-  error_message = ''
-  if request.method == 'POST':
-    # Create a 'user' form object that includes the data from the browser
-    form = SignUpForm(request.POST)
-    if form.is_valid():
-      # Add the user to the database
-      form.type = 'renter'
-      user = form.save()
-      # Login after signing up
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = SignUpForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
-
-# class CreateProfile(CreateView):
-#   model = Profile
-#   template_name = 'profile/createprofile.html'
-#   fields = ['first_name', 'last_name', 'prof']
 
 def create_profile(request):
   return render(request, 'profile/createprofile.html')
