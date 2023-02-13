7f8b58c6942f8d81971070bc524be2b924a8a032
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2023-02-13 09:28:23 -0500

    Fix signup form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -8,14 +8,17 @@ from main_app.models import Availability, Review,Like
 
 
 class NewUserForm(UserCreationForm):
-    # email = forms.EmailField(required=True)
-    # first_name = forms.CharField(max_length=50, required=True)
-    # last_name = forms.CharField(max_length=50, required=True)
+    username = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))  
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput(attrs={'class': 'form-control'})) 
     
     def __init__(self, *args, **kwargs):
         super(NewUserForm, self).__init__(*args, **kwargs)
 
-        for fieldname in ['username', 'password1', 'password2']:
+        for fieldname in ['username', "password1", "password2"]:
             self.fields[fieldname].help_text = None
 
     class Meta:
@@ -23,11 +26,11 @@ class NewUserForm(UserCreationForm):
         fields = ("username", "first_name", "last_name", "email", "password1", "password2")
         widgets = {
             'username' : forms.TextInput(attrs={'class': 'form-control'}),
-            'first_name' : forms.TextInput(attrs={'class': 'form-control'}),
-            'last_name' : forms.TextInput(attrs={'class': 'form-control'}),
-            'email' : forms.EmailInput(attrs={'class': 'form-control'}),
-            'password1' : forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2' : forms.PasswordInput(attrs={'class': 'form-control'}),
+            # 'first_name' : forms.TextInput(attrs={'class': 'form-control'}),
+            # 'last_name' : forms.TextInput(attrs={'class': 'form-control'}),
+            # 'email' : forms.EmailInput(attrs={'class': 'form-control'}),
+            # 'password' : forms.PasswordInput(attrs={'class': 'form-control'}),
+            # 'confirm_password' : forms.PasswordInput(attrs={'class': 'form-control'}),
         }
 
 
